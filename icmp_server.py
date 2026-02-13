f8b2726019f1b542140f74087454ce0d66a592aa
hkspark <77894884+hkspark@users.noreply.github.com>
2026-02-12 19:27:13 -0500

    Update ICMP packet sniffing logic
    Refactor packet sniffing to continuously process ICMP packets.

--- icmp_server.py
+++ icmp_server.py
@@ -42,12 +42,9 @@ client_ip = "192.168.19.131"
 packet = IP(dst=client_ip)/ICMP()/Raw(load=b"PUB:" + public_bytes)
 send(packet, verbose =0)
 
-<<<<<<< HEAD
 while True:
   sniff(filter="icmp and src=" + client_ip, prn=get_packets, count = 1)
   packet = IP(dst=client_ip)/ICMP()/Raw(load="MSG: connected")
   send(packet, verbose = 0)
-=======
 #Continue to sniff for ICMP packets and process them as they come in
 sniff(filter="icmp", prn=get_packets)
->>>>>>> 8251378a4962abea48dbcf29d81914ea47da891a
