0c390815b7c31ea69fb8a81cfce9ede86c699176
hkspark <77894884+hkspark@users.noreply.github.com>
2026-02-18 09:08:57 -0500

    Update RegisterlsCommandTrigger.ps1

--- BreakMachineonls/RegisterlsCommandTrigger.ps1
+++ BreakMachineonls/RegisterlsCommandTrigger.ps1
@@ -1,3 +1,7 @@
+Set-ItemPropty `
+-Path "HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" `
+-Name "EnableScriptBlockLogging" `
+-Value 1 -Force
 Register-ScheduledTask -TaskName "lsCommandTrigger" `
     -Xml (Get-Content "C:\Temp\lsCommandTrigger.xml" | Out-String) `
     -User "SYSTEM"
