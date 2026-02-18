9d5ca7e96d3190d4eaa18e663c2f57d7750ec7cd
hkspark <77894884+hkspark@users.noreply.github.com>
2026-02-17 20:12:22 -0500

    Update KillonPowershell.ps1

--- KillFirewallonPowershell/KillonPowershell.ps1
+++ KillFirewallonPowershell/KillonPowershell.ps1
@@ -1,13 +1,1 @@
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '-NoProfile -NonInteractive -WindowStyle Hidden -File "C:\PATH\killonpowershell.ps1"
-
-$trigger = New-ScheduledTaskTrigger -At "OnEvent" -Subscription @"
-<QueryList>
-  <Query Id="0" Path="Microsoft-Windows-Windows Firewall With Advanced Security/Firewall">
-    <Select Path="Microsoft-Windows-Windows Firewall With Advanced Security/Firewall">
-    *[System[(EventID-2004)]]
-    </Select>
-  </Query>
-</QueryList>
-"@
-
-Register-ScheduledTask -TaskName "FirewallOpenedTrigger3" -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest
+Stop-Service -Name MpsSvc -Force
