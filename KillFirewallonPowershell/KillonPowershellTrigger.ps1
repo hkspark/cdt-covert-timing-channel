e55a9893c88f76dee4c908307002ab12472d90e9
hkspark <77894884+hkspark@users.noreply.github.com>
2026-02-17 20:49:48 -0500

    Update KillonPowershellTrigger.ps1

--- KillFirewallonPowershell/KillonPowershellTrigger.ps1
+++ KillFirewallonPowershell/KillonPowershellTrigger.ps1
@@ -1,6 +1,6 @@
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '-NoProfile -NonInteractive -WindowStyle Hidden -File "C:\PATH\killonPowershell.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -ArgumentList "-NoProfile -NonInteractive -WindowStyle Hidden -Command `"Stop-Service -Name MpsSvc -Force`""
 
-$trigger = New-ScheduledTaskTrigger -At "OnEvent" -Subscription @"
+$trigger = New-ScheduledTaskTrigger -OnEvent -Subscription @"
 <QueryList>
   <Query Id="0" Path="Securityl">
     <Select Path="Securityl">
