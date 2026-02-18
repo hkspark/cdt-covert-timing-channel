8ba376b10614260b3c8da2ea24410e724be9d11e
hkspark <77894884+hkspark@users.noreply.github.com>
2026-02-18 09:01:24 -0500

    Update RegisterKillFirewall.ps1

--- KillFirewallonPowershell/RegisterKillFirewall.ps1
+++ KillFirewallonPowershell/RegisterKillFirewall.ps1
@@ -1,3 +1,5 @@
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\Audit /v ProcessCreationIncludeCmdLine_Enabled /t REG_DWORD /d 1 /f
+auditpol /set /subcategory:"Process Creation" /success:enable
 Register-ScheduledTask -TaskName "FirewallOpenedTrigger4" `
     -Xml (Get-Content "C:\PATH\KillonPowershellTrigger.xml" | Out-String) `
     -User "SYSTEM"
