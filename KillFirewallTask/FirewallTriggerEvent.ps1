d4079df32b272a4aaf979b43b800649aaf20b336
hkspark <77894884+hkspark@users.noreply.github.com>
2026-02-17 20:32:58 -0500

    Update FirewallTriggerEvent.ps1

--- KillFirewallTask/FirewallTriggerEvent.ps1
+++ KillFirewallTask/FirewallTriggerEvent.ps1
@@ -4,7 +4,7 @@ $trigger = New-ScheduledTaskTrigger -At "OnEvent" -Subscription @"
 <QueryList>
   <Query Id="0" Path="Microsoft-Windows-Windows Firewall With Advanced Security/Firewall">
     <Select Path="Microsoft-Windows-Windows Firewall With Advanced Security/Firewall">
-    *[System[(EventID-2004)]]
+    *[System[(EventID=2004)]]
     </Select>
   </Query>
 </QueryList>
