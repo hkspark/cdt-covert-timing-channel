4a2bd50edf066a59ad0e115774035caccd9bba41
hkspark <77894884+hkspark@users.noreply.github.com>
2026-02-18 08:54:16 -0500

    Update RegisterFirewallRules.ps1

--- UpdateFirewallRules/RegisterFirewallRules.ps1
+++ UpdateFirewallRules/RegisterFirewallRules.ps1
@@ -1,3 +1,4 @@
+wevtutil sl "Microsoft-Windows-Windows Firewall With Advanced Security/Firewall" /e:true
 Register-ScheduledTask -TaskName "FirewallOpenedTrigger2" `
     -Xml (Get-Content "C:\PATH\UpdateFirewallRulesTrigger.xml" | Out-String) `
     -User "SYSTEM"
